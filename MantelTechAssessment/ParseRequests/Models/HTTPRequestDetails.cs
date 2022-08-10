c7ba4e0f355579df9dc1ea187a7b2a95b584b3ca
@Pandorux <jared.l.j.carey@gmail.com>
2022-08-11 07:51:06 +1000

    Added New HTTPRequestDetails Model and Added New Regex in Helper

---
+++ MantelTechAssessment/ParseRequests/Models/HTTPRequestDetails.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParseRequests.Models
+{
+    public class HTTPRequestDetails
+    {
+        public readonly string HTTPRequest;
+        public DateTime TimeOfRequest { get; private set; }
+
+        HTTPRequestDetails(string HTTPRequest, DateTime TimeOfRequest)
+        {
+            this.HTTPRequest = HTTPRequest;
+            this.TimeOfRequest = TimeOfRequest;
+        }
+    }
+}
