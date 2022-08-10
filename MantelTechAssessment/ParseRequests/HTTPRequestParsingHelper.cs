1ec845f41226859803e6e54bb0a0c6b685751f3a
@Pandorux <jared.l.j.carey@gmail.com>
2022-08-11 07:04:06 +1000

    Added Extracting of IP Address

---
+++ MantelTechAssessment/ParseRequests/HTTPRequestParsingHelper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ParseRequests
+{
+    static class HTTPRequestParsingHelper
+    {
+        // Regex expression for validating IP
+        public static readonly Regex IP_REGEX = new Regex("([0-9]{1,3}\\.|\\*\\.){3}([0-9]{1,3}|\\*){1}");
+    }
+}
--- MantelTechAssessment/ParseRequests/Program.cs
+++ MantelTechAssessment/ParseRequests/Program.cs
@@ -1,5 +1,7 @@
 ﻿
 // See https://aka.ms/new-console-template for more information
+using ParseRequests;
+
 Console.WriteLine(@"
     Hello, This is the Mantel Tech Assessment!
     
@@ -9,9 +11,26 @@ Console.WriteLine(@"
 
 const string fileName = "programming-task-example-data_(1).log";
 
-
 List<string> lines = File.ReadAllLines("../../../" + fileName).ToList();
 lines.ForEach(l => Console.WriteLine(l));
+
+List<string> ips = new List<string>();
+lines.ForEach(l =>
+{
+    string result = HTTPRequestParsingHelper.IP_REGEX.Match(l).Value;
+
+    if (!String.IsNullOrEmpty(result))
+    {
+        ips.Add(result);
+    }
+    else
+    {
+        throw new Exception("IP Not Found");
+    }
+});
+
+ips.ForEach(l => Console.WriteLine(l));
+Console.WriteLine($"There are {ips.Count} IP Addresses Provided");
 
 Console.WriteLine(@"
 
