41f33d4e367c8f67fd0d7569938c9cba4cf7835f
@Pandorux <jared.l.j.carey@gmail.com>
2022-08-11 06:40:02 +1000

    Read and Logging of File Working, Adjusted Project Settings

---
+++ MantelTechAssessment/ParseRequests/Program.cs
@@ -0,0 +1,18 @@
+﻿
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine(@"
+    Hello, This is the Mantel Tech Assessment!
+    
+    The purpose of this Tech Assessment will be to parse HTTP Requests and list details the aforementioned HTTP Requests. \n\n
+");
+
+
+const string fileName = "programming-task-example-data_(1).log";
+
+
+List<string> lines = File.ReadAllLines("../../../" + fileName).ToList();
+lines.ForEach(l => Console.WriteLine(l));
+
+Console.WriteLine(@"
+
+    And that is the entire file logged above");
